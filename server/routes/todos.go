8bcdbb2fd2005cf5c2fdc43f5a9c064181127d13
HakanCava <hakancavaa@gmail.com>
2024-10-17 23:32:55 +0300

    feat: api funcs created

--- server/routes/todos.go
+++ server/routes/todos.go
@@ -1,9 +1,25 @@
 package routes
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 var validate = validator.New()
 var todoCollection *mongo.Collection = OpenCollection(Client, "todos")
+
+func CreateTodo(c *gin.Context) {}
+
+func UpdateTodo(c *gin.Context) {}
+
+func GetTodos(c *gin.Context) {}
+
+func GetTodoById(c *gin.Context) {}
+
+func DeleteTodo(c *gin.Context) {}
+
+func GetTodosFromTrash(c *gin.Context) {}
+
+func UpdateTodoFromTrash(c *gin.Context) {}
+
